ae2f1d8ce8500dcf5dd08b92d02a3753b120bf2e
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-05-11 15:22:26 -0500

    added homework notes 20210511 form element

---
+++ src/app/to-do-item/to-do-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-item',
+  templateUrl: './to-do-item.component.html',
+  styleUrls: ['./to-do-item.component.scss']
+})
+export class ToDoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-to-do',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoComponent implements OnInit {
 
-  constructor() { }
+/// Properties are usually camelCase
 
+  @Input() title: string = 'john jacob jingleheimer';
+  toDos: string[]=[]
+  /// ^ this initializes an empty array (the equals part)
+  newItemName: string = 'jingleheimer John';
+  /// @Input() newItemName: string = 'jingleheimer John';
+  constructor() { }
   ngOnInit(): void {
   }
-
 }
